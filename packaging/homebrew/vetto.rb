022e726c7720306b35da898502ef6138c7227dde
Shleder <maksboreichuk88@gmail.com>
2026-09-19 23:40:58 +0300

    chore(packaging): sync homebrew formula checksums for v0.3.2

--- packaging/homebrew/vetto.rb
+++ packaging/homebrew/vetto.rb
@@ -7,20 +7,20 @@ class Vetto < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/shleder/vetto/releases/download/v0.3.2/vetto-macos-aarch64.tar.gz"
-      sha256 "68a8adf34199fab509d76f2e089b9a67dc4652857748ed8648a082bbf9f8c806"
+      sha256 "44be7f5a852cb70a1fa6165b10ba9741ee992b90fe2bd7be924eb4e26885c937"
     else
       url "https://github.com/shleder/vetto/releases/download/v0.3.2/vetto-macos-x86_64.tar.gz"
-      sha256 "96d92c40d148aa73771a7146bd1db819c035a5540cd10860a9a82200a0ced071"
+      sha256 "b19ef0834ec45694a771fb291452471ad308fe457ea308f7c6a719c93f44830a"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/shleder/vetto/releases/download/v0.3.2/vetto-linux-aarch64.tar.gz"
-      sha256 "8a63b9a8097b9eb622e9458164006d868507de2b3f822a089e0e501ef6b2a367"
+      sha256 "fa545eecdd2b84d7bd6b8ef3665cfe699fe5a1b0450877aa450bec397d8300cf"
     else
       url "https://github.com/shleder/vetto/releases/download/v0.3.2/vetto-linux-x86_64.tar.gz"
-      sha256 "3e047e6f7664912df2de49dfdf7845b5f43de6d45b1ff504c3aa87e11c0f3de8"
+      sha256 "764d908bb99dd8dab76027dd69c5e502876d8233bcb23c6984451ae1e83c548e"
     end
   end
 
